efdb5106ed682d951af98f9a10b8f222d5bcb6e3
Kaua Silva <kauassilva1@gmail.com>
2023-11-05 18:46:43 -0300

    insert, delete and update method to Client

--- src/main/java/com/switzerlandbank/api/resources/ClientResource.java
+++ src/main/java/com/switzerlandbank/api/resources/ClientResource.java
@@ -1,13 +1,19 @@
 package com.switzerlandbank.api.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.switzerlandbank.api.entities.Client;
 import com.switzerlandbank.api.services.ClientService;
@@ -30,5 +36,25 @@ public class ClientResource {
 		Client obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@PostMapping
+	public ResponseEntity<Client> insert(@RequestBody Client obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Client> update(@RequestBody Client obj, @PathVariable Long id) {
+		obj = service.update(obj, id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 
 }
--- src/main/java/com/switzerlandbank/api/services/ClientService.java
+++ src/main/java/com/switzerlandbank/api/services/ClientService.java
@@ -4,11 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.switzerlandbank.api.entities.Client;
 import com.switzerlandbank.api.repositories.ClientRepository;
+import com.switzerlandbank.api.services.exceptions.DatabaseException;
 import com.switzerlandbank.api.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ClientService {
@@ -23,6 +27,41 @@ public class ClientService {
 	public Client findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	public Client insert(Client obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		if (!repository.existsById(id)) {
+			throw new ResourceNotFoundException(id);		
+		}
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Client update(Client obj, Long id) {
+		try {
+			Client entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);			
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	
+	private void updateData(Client entity, Client obj) {
+		entity.setName(obj.getName());
+		entity.setCpf(obj.getCpf());
+		entity.setMotherName(obj.getMotherName());
+		entity.setDateBirth(obj.getDateBirth());
+		entity.setGender(obj.getGender());
+		entity.setEmail(obj.getEmail());
+		entity.setPassword(obj.getPassword());
 	}
 
 }
