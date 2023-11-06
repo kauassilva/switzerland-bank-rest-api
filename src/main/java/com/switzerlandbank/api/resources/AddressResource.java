adffbc38210cc51b253718a67e77d1922ec588e5
Kaua Silva <kauassilva1@gmail.com>
2023-11-05 23:24:08 -0300

    Added insert, delete and update methods to Address entity

--- src/main/java/com/switzerlandbank/api/resources/AddressResource.java
+++ src/main/java/com/switzerlandbank/api/resources/AddressResource.java
@@ -1,13 +1,19 @@
 package com.switzerlandbank.api.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.switzerlandbank.api.entities.Address;
 import com.switzerlandbank.api.services.AddressService;
@@ -27,7 +33,26 @@ public class AddressResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Address> findById(@PathVariable Long id) {
-		Address obj = service.finById(id);
+		Address obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Address> insert(@RequestBody Address obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Address> update(@RequestBody Address obj, @PathVariable Long id) {
+		obj = service.update(obj, id);
 		return ResponseEntity.ok().body(obj);
 	}
 
--- src/main/java/com/switzerlandbank/api/services/AddressService.java
+++ src/main/java/com/switzerlandbank/api/services/AddressService.java
@@ -4,11 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.switzerlandbank.api.entities.Address;
 import com.switzerlandbank.api.repositories.AddressRepository;
+import com.switzerlandbank.api.services.exceptions.DatabaseException;
 import com.switzerlandbank.api.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class AddressService {
@@ -20,9 +24,42 @@ public class AddressService {
 		return repository.findAll();
 	}
 	
-	public Address finById(Long id) {
+	public Address findById(Long id) {
 		Optional<Address> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+	
+	public Address insert(Address obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		if (!repository.existsById(id)) {
+			throw new ResourceNotFoundException(id);
+		}
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Address update(Address obj, Long id) {
+		try {
+			Address entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);			
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Address entity, Address obj) {
+		entity.setStreet(obj.getStreet());
+		entity.setNeighborhood(obj.getNeighborhood());
+		entity.setCity(obj.getCity());
+		entity.setState(obj.getState());
+		entity.setPostalCode(obj.getPostalCode());
 	}
 
 }
