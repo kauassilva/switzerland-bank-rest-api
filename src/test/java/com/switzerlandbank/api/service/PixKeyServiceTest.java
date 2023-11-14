1f66bf1e4012770b9a51382ec30bc56ad9f5862f
Kaua Silva <kauassilva1@gmail.com>
2023-11-14 14:56:11 -0300

    Created PixKeyServiceTest

---
+++ src/test/java/com/switzerlandbank/api/service/PixKeyServiceTest.java
@@ -0,0 +1,14 @@
+package com.switzerlandbank.api.service;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class PixKeyServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
