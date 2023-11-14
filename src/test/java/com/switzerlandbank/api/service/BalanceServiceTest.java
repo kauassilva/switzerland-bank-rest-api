73435ec4dd79983204ff7fda8825aafc2ccecd1b
Kaua Silva <kauassilva1@gmail.com>
2023-11-13 22:46:31 -0300

    Teste do Balance part 2

--- src/test/java/com/switzerlandbank/api/service/BalanceServiceTest.java
+++ src/test/java/com/switzerlandbank/api/service/BalanceServiceTest.java
@@ -1,6 +1,5 @@
 package com.switzerlandbank.api.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -13,14 +12,11 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-import org.hibernate.mapping.Collection;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.switzerlandbank.api.entities.Balance;
@@ -28,10 +24,9 @@ import com.switzerlandbank.api.repositories.BalanceRepository;
 import com.switzerlandbank.api.services.exceptions.ResourceNotFoundException;
 import com.switzerlandbank.api.services.impls.BalanceServiceImpl;
 
-@ExtendWith(MockitoExtension.class)
-public class BalanceServiceTest {
+class BalanceServiceTest {
 
-    @Mock
+	@Mock
     private BalanceRepository repository;
 
     @InjectMocks
@@ -77,13 +72,11 @@ public class BalanceServiceTest {
 
     @Test
     void testFindByIdButTheresNoId(){
-         Balance balanco = new Balance(1L, new BigDecimal(3), Instant.now(), null);
-
-        when(repository.findById(1L)).thenReturn(Optional.of(balanco));
-        assertThrows( ResourceNotFoundException.class, () -> 
-            service.findById(6L));
+        when(repository.findById(1L)).thenReturn(Optional.of(balance));
+        assertThrows(ResourceNotFoundException.class, () -> service.findById(6L));
     
         //assertThat(e.getMessage().contains("Não a id"));
 
     }
+
 }
