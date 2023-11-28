b39ab847124d4f5191eab8a22fa7b8f6305f488e
Jullia <137806375+julliakathelyn@users.noreply.github.com>
2023-11-27 21:12:09 -0300

    test integration repository completos

--- src/test/java/com/switzerlandbank/api/repositories/integrations/AccountRepositoryIntegrationTest.java
+++ src/test/java/com/switzerlandbank/api/repositories/integrations/AccountRepositoryIntegrationTest.java
@@ -1,5 +1,7 @@
 package com.switzerlandbank.api.repositories.integrations;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
@@ -11,12 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.orm.jpa.JpaSystemException;
 
 import com.switzerlandbank.api.entities.Account;
 import com.switzerlandbank.api.entities.Address;
 import com.switzerlandbank.api.entities.Customer;
 import com.switzerlandbank.api.entities.enums.Gender;
 import com.switzerlandbank.api.repositories.AccountRepository;
+
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
@@ -28,9 +32,12 @@ public class AccountRepositoryIntegrationTest {
 
     private Account account;
 
+    private Customer customer;
+
     @BeforeEach
     void setUp() {
-        Customer customer = new Customer(null, "João Silva", "12345678910", "Maria Silva", LocalDate.parse("1980-07-15"),
+        customer = new Customer(null, "João Silva", "12345678910", "Maria Silva",
+                LocalDate.parse("1980-07-15"),
                 Gender.MALE, "joaosilva@example.com", "JoaoSilva123");
         Address address = new Address(null, "Av. Castelo Branco", "1416", "Centro", "Paraíso do Tocantins", "Tocantins",
                 "77600000", customer);
@@ -38,7 +45,6 @@ public class AccountRepositoryIntegrationTest {
 
         account = new Account(null, null, customer);
 
-        
     }
 
     @Test
@@ -48,6 +54,40 @@ public class AccountRepositoryIntegrationTest {
         Optional<Account> foundAccount = accountRepository.findById(savedAccount.getId());
 
         assertTrue(foundAccount.isPresent());
+    }
+
+    @Test
+    void testFindById_NotFound() {
+        Account savedAccount = accountRepository.save(account);
+
+        Optional<Account> foundAccount = accountRepository.findById(savedAccount.getId() + 1L);
+
+        assertTrue(foundAccount.isEmpty());
+    }
+
+    @Test
+    void testSave_Success() {
+        Account savedAccount = accountRepository.save(account);
+
+        assertEquals(account, savedAccount);
+    }
+
+    @Test
+    void testSave_ThrowsConstraintViolationException() {
+        Account account = new Account(null, null, null);
+
+        assertThrows(JpaSystemException.class, () -> accountRepository.save(account));
+    }
+
+    @Test
+    void testDelete_Success() {
+        Account savedAccount = accountRepository.save(account);
+
+        accountRepository.deleteById(savedAccount.getId());
+
+        Optional<Account> foundAccount = accountRepository.findById(savedAccount.getId());
+
+        assertTrue(foundAccount.isEmpty());
     }
 
 }
